16fc0906172a52428e20c798f1cea126aa5da6ba
Ryan Latta <recursive.faults@gmail.com>
2016-06-24 10:48:18 -0500

    Add basic premake scripting.

---
+++ premake4.lua
@@ -0,0 +1,22 @@
+-- A solution contains projects, and defines the available configurations
+solution "Inaction"
+    configurations { "Debug", "Release" }
+    includedirs {"/usr/local/include", "include", "lib"}
+    links {"SDL2", "SDL2_image", "SDL2_ttf", "SDL2_mixer" }
+    libdirs {"/usr/local/lib", "lib"}
+    kind "ConsoleApp"
+    buildoptions {"-Wall", "-std=c++11" }
+    language "C++"
+
+    -- A project defines one build target
+    project "inaction"
+    files { "src/**.h", "src/**.cpp" }
+    excludes { "test/*" }
+
+    configuration "Debug"
+    defines { "DEBUG" }
+    flags { "Symbols" }
+
+    configuration "Release"
+    defines { "NDEBUG" }
+    flags { "Optimize" }
